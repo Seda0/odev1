6f5f58c4bb50aed45f9a2d8a648202fef594bb86
Sirin Saglik <sdsaglik@gmail.com>
2024-12-30 14:26:48 +0300

    second

--- odev1.dart
+++ odev1.dart
@@ -7,6 +7,25 @@ void main() {
  dynamic e= 5;
   e=16;
   
-  print('a= ${a} \n b=${b} \n c=${c}  \n d=${d} \n e=${e}');
+  print('a= ${a} \n b=${b} \n c=${c}  \n d=${d} \n e=${e} \n');
+
+
+int myAge= 22;                 //CamelCase 
+double my_height = 1.65;        //snake_cas
+String myName = "Şirin";     //CamelCase
+bool YetiskinMi = true;     //PascalCase
+dynamic my_weight = 05;    //snake_cas
+       my_weight =09;
+
+  print('myAge= ${myAge} \n my_height=${my_height} \n myName=${myName}  \n YetiskinMi=${YetiskinMi} \n my_weight=${my_weight} \n');
+
+  String ad = "Şirin Seda";
+  var soyad = "Sağlık";
+  int yas = 22;
+  bool resitMi = true;
   
+    print('ad= ${ad} \n soyad=${soyad} \n yas=${yas}  \n resitMi=${resitMi} \n ');
+
+
+
 }
